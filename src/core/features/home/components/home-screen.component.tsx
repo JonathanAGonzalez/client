cf6280d0962132fd1241dde5f2505377b3e94f31
Jona <jonathang@hotmail.com.ar>
2024-09-27 15:37:19 -0300

    feat: add HomeScreen component with a thank you message and styling

---
+++ src/core/features/home/components/home-screen.component.tsx
@@ -0,0 +1,10 @@
+export const HomeScreen = () => {
+  return (
+    <div className='text-center py-4'>
+      <h2>
+        Más allá del resultado, fue un placer participar en este desafío 🚀
+      </h2>
+      <p>Muchas gracias por la oportunidad ❤️ </p>
+    </div>
+  );
+};
