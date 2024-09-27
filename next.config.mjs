ffc513220b99ab62fa4049dd120982901c4fb65f
Jona <jonathang@hotmail.com.ar>
2024-09-27 15:18:31 -0300

    feat: add dependencies (axios sas ) and configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  sassOptions: {
+    implementation: 'sass-embedded',
+  },
+  images: {
+    domains: ['http2.mlstatic.com'],
+  },
+};
 
 export default nextConfig;
